99d40eb37317ac23d613a574d9d85698bc667b7a
Demaris Ashmore <demiashmore@yahoo.com>
2022-12-27 13:02:58 -0700

    Seeded tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,34 @@
+puts "Clearing tables..."
+
+Day.destroy_all
+Task.destroy_all
+
+puts "All clear!"
+
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+
+## Day seeds
+sunday = Day.create(name: "Sunday")
+monday = Day.create(name: "Monday")
+tuesday = Day.create(name: "Tuesday")
+wednesday = Day.create(name: "Wednesday")
+thursday = Day.create(name: "Thursday")
+friday = Day.create(name: "Friday")
+saturday = Day.create(name: "Saturday")
+
+##Task seeds
+grocery_shopping = Task.create(task_name: "Grocery shooping", day_id: wednesday.id)
+oil_change = Task.create(task_name: "Get oil change for car", day_id: saturday.id)
+car_wash = Task.create(task_name: "Wash car", day_id: saturday.id)
+laundry = Task.create(task_name: "Wash laundry", day_id: sunday.id)
+clean_house = Task.create(task_name: "Clean house", day_id: sunday.id)
+therapy = Task.create(task_name: "Therapy appointment", day_id: tuesday.id)
+date_night = Task.create(task_name: "Date night with husband", day_id: friday.id)
+dog_park = Task.create(task_name: "Take dog to park", day_id: saturday.id)
+meal_plan = Task.create(task_name: "Plan meals for week", day_id: monday.id)
+water_plants = Task.create(task_name: "Water plants", day_id: thursday.id)
+
 
 puts "✅ Done seeding!"
