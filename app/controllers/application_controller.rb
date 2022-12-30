fa0d08dd0e821fd258be2ccfe7c5c6eefdd46a85
Demaris Ashmore <demiashmore@yahoo.com>
2022-12-29 17:05:56 -0700

    Deleted unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < Sinatra::Base
 
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
-  
   get "/days" do
     Day.all.to_json
   end
